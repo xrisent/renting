2b3b7b137323c2a58842147e83f9e50977489249
xrisent <ormonov31261@gmail.com>
2025-03-20 18:46:25 +0600

    ssr in carPage

--- src/app/cars/[id]/page.tsx
+++ src/app/cars/[id]/page.tsx
@@ -1,48 +1,38 @@
-"use client";
-
-import React, { useEffect, useState } from "react";
+import React from "react";
+import './singleCar.scss'
 import CarDescription from "@/components/CarDescription/CarDescription";
-import "./singleCar.scss";
 import { Banner } from "@/components/Banner/Banner";
 import FormForRent from "@/components/FormForRent/FormForRent";
-import { useParams } from "next/navigation";
 import CarPhotos from "@/components/CarPhotos/CarPhotos";
 
-const Car: React.FC = () => {
-    const { id } = useParams();
-    
-    const [carData, setCarData] = useState<any>(null);
+import { fetchCarData } from "@/features/fetchCarData/fetchCarData";
 
-    useEffect(() => {
-        async function fetchCar() {
-            try {
-                const res = await fetch(`/api/cars?id=${id}`);
-                const data = await res.json();
-                setCarData(data);
-            } catch (error) {
-                console.error("Ошибка загрузки данных:", error);
-            }
-        }
 
-        fetchCar();
-    }, [id]);
+
+const Car = async ({ params }: { params: { id: string } }) => {
+    const { id } = await params; 
+
+    let carData = null;
+    try {
+        carData = await fetchCarData(id);
+    } catch (error) {
+        console.error("Ошибка загрузки данных:", error);
+    }
 
     if (!carData) {
         return <div>Loading...</div>;
     }
 
-    console.log(carData[0].mainImage)
-
     return (
         <>
-            <Banner image={carData[0].mainImage} h3="Аренда автомобилей"/>
+            <Banner image={carData[0].mainImage} h3="Аренда автомобилей" />
             <section id="singleCarSection">
                 <div className="container">
                     <div className="singleCarSection__box">
                         <div className="singleCarSection__box__info">
                             <CarDescription car={carData[0]} />
                             <hr />
-                            <CarPhotos car={carData[0]}/>
+                            <CarPhotos car={carData[0]} />
                         </div>
                         <FormForRent />
                     </div>
---
+++ src/features/fetchCarData/fetchCarData.tsx
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export async function fetchCarData(id: string) {
+    try {
+        const response = await axios.get(`http://localhost:3000/api/cars`, { params: { id } });
+        return response.data;
+    } catch (error) {
+        throw new Error("Ошибка загрузки данных");
+    }
+}
