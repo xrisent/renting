113e1d2a9c41df39821f663b44c4fac0dc0ac18f
xrisent <ormonov31261@gmail.com>
2025-04-03 16:33:41 +0600

    fix switcher

--- src/shared/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/shared/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,8 +1,19 @@
-import { useRouter } from "next/navigation";
-import './LanguageSwitcher.scss'
+import { useRouter, useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import './LanguageSwitcher.scss';
 
 export const LanguageSwitcher: React.FC<{}> = () => {
     const router = useRouter();
+    const { locale } = useParams();
+    const [currentLocale, setCurrentLocale] = useState<string>("ru");
+
+    useEffect(() => {
+        if (typeof locale === 'string') {
+            setCurrentLocale(locale); 
+        } else if (Array.isArray(locale)) {
+            setCurrentLocale(locale[0]); 
+        }
+    }, [locale]);
 
     const handleLocaleChange = (locale: string) => {
         router.push(`/${locale}`);
@@ -10,7 +21,10 @@ export const LanguageSwitcher: React.FC<{}> = () => {
 
     return (
         <div className="languageSwitcher">
-            <select onChange={(e) => handleLocaleChange(e.target.value)} defaultValue="ru">
+            <select
+                onChange={(e) => handleLocaleChange(e.target.value)}
+                value={currentLocale}
+            >
                 <option value="ru">RU</option>
                 <option value="en">EN</option>
             </select>
